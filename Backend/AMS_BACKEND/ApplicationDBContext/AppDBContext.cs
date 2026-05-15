030ebc82feea71dc5355f9781f4fb29857cf8669
kryllejaneracaza <kryllejane.racaza@dbtc-cebu.edu.ph>
2026-05-15 19:12:10 +0800

    Modify: AppDBContext

--- Backend/AMS_BACKEND/ApplicationDBContext/AppDBContext.cs
+++ Backend/AMS_BACKEND/ApplicationDBContext/AppDBContext.cs
@@ -1,28 +1,18 @@
 ﻿using AMS_BACKEND.Models;
 using Microsoft.EntityFrameworkCore;
-
+using System.Collections.Generic;
+using System.Reflection.Emit;
 namespace AMS_BACKEND.ApplicationDBContext
 {
     public class AppDBContext : DbContext
     {
         public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
-
         public DbSet<Student> Students { get; set; }
         public DbSet<Teacher> Teachers { get; set; }
         public DbSet<Course> Courses { get; set; }
         public DbSet<Attendance> Attendances { get; set; }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Student>()
-                .HasKey(s => s.StudentId);
-
-            modelBuilder.Entity<Teacher>()
-                .HasKey(t => t.TeacherId);
-
-            modelBuilder.Entity<Course>()
-                .HasKey(c => c.CourseCode);
-
             modelBuilder.Entity<Attendance>()
                 .HasKey(a => new { a.StudentId, a.CourseCode, a.Date });
         }
